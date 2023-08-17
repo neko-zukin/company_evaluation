d878a5bbe75c3b0f6a6596977ff16fabd0ecc63f
Gummy <k-kato@ciel-japan.jp>
2023-08-17 16:42:13 +0900

    sql

---
+++ code/DB/send.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-type: text/plain; charset=UTF-8");
+
+try{
+	// 接続
+	$db = new PDO('mysql:host=localhost;dbname=company_money_maneger', 'root', 'P@ssw0rd');
+
+	$sql = $_POST['sql'];
+	$result = $db->query($sql);
+    $arysql = $result -> fetchAll(PDO::FETCH_ASSOC);
+    // echoすると返せる
+    echo json_encode($arysql, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT); // json形式にして返す
+ 
+	// 切断
+	$db = null;
+} catch(PDOException $e){
+    echo "データベース接続失敗" . PHP_EOL;
+	echo $e->getMessage();
+	exit;
+}
+?>
